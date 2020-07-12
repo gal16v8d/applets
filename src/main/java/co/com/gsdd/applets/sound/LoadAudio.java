3a0c92726ffa1c729bd95728dc78184694f3fa85
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-07-12 17:41:40 -0500

    Update applet samples

---
+++ src/main/java/co/com/gsdd/applets/sound/LoadAudio.java
@@ -0,0 +1,49 @@
+package co.com.gsdd.applets.sound;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoadAudio extends Applet {
+
+	private static final long serialVersionUID = -6483320475550879244L;
+	private AudioClip currentClip;
+	private Choice selectSound;
+
+	public void init() {
+		Map<String, AudioClip> audioClips = new HashMap<>();
+		audioClips.put("Raining", getAudioClip(getDocumentBase(), "Raining in paradize.wav"));
+		audioClips.put("Hi", getAudioClip(getDocumentBase(), "hi.au"));
+		audioClips.put("Welcome", getAudioClip(getDocumentBase(), "welcome.wav"));
+		
+		selectSound = new Choice();
+		audioClips.keySet().stream().forEach(key -> selectSound.add(key));
+		add(selectSound);
+
+		selectSound.addItemListener((ItemEvent e) -> {
+			currentClip.stop();
+			currentClip = audioClips.getOrDefault(selectSound.getSelectedItem(), currentClip);
+		});
+
+		Button play = new Button("Play");
+		play.addActionListener((ActionEvent e) -> currentClip.play());
+		add(play);
+		Button loop = new Button("Loop");
+		loop.addActionListener((ActionEvent e) -> currentClip.loop());
+		add(loop);
+		Button stop = new Button("Stop");
+		stop.addActionListener((ActionEvent e) -> currentClip.stop());
+		add(stop);
+		currentClip = audioClips.get("Raining");
+	}
+
+	public void stop() {
+		currentClip.stop();
+	}
+	
+}
